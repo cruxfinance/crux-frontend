9fc33a498ede015c210046f2764f296bdeba744d
esot321c <esot321c@gmail.com>
2023-08-23 14:26:01 -0700

    remove mock data apis

--- src/pages/api/mocks/tokens/[tokenId].ts
+++ src/pages/api/mocks/tokens/[tokenId].ts
@@ -1,36 +1,36 @@
-import { tokenData } from '@pages/api/mocks/data/tokenInfo';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { tokenData } from '@pages/api/mocks/data/tokenInfo';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-// Hard-coded exchange rates for testing
-const exchangeRates = {
-  USD: 1,
-  ERG: 0.83
-};
+// // Hard-coded exchange rates for testing
+// const exchangeRates = {
+//   USD: 1,
+//   ERG: 0.83
+// };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const tokenId = req.query.tokenId
-  const currency = req.query.currency
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const tokenId = req.query.tokenId
+//   const currency = req.query.currency
 
-  if (typeof tokenId !== 'string') {
-    return res.status(400).json({ error: 'Invalid token ID' });
-  }
+//   if (typeof tokenId !== 'string') {
+//     return res.status(400).json({ error: 'Invalid token ID' });
+//   }
 
-  if (currency && (!exchangeRates[currency as keyof typeof exchangeRates] || typeof currency !== 'string')) {
-    return res.status(400).json({ error: 'Invalid currency' });
-  }
+//   if (currency && (!exchangeRates[currency as keyof typeof exchangeRates] || typeof currency !== 'string')) {
+//     return res.status(400).json({ error: 'Invalid currency' });
+//   }
 
-  // Find the token by its tokenId
-  const token = tokenData.find(t => t.tokenId === tokenId);
+//   // Find the token by its tokenId
+//   const token = tokenData.find(t => t.tokenId === tokenId);
 
-  if (!token) {
-    return res.status(404).json({ error: 'Token not found' });
-  }
+//   if (!token) {
+//     return res.status(404).json({ error: 'Token not found' });
+//   }
 
-  const rate = exchangeRates[currency as keyof typeof exchangeRates];
-  const newToken = {
-    ...token,
-    price: token.price * rate
-  }
+//   const rate = exchangeRates[currency as keyof typeof exchangeRates];
+//   const newToken = {
+//     ...token,
+//     price: token.price * rate
+//   }
 
-  return res.status(200).json(newToken);
-}
+//   return res.status(200).json(newToken);
+// }
