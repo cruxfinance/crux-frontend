ece35d10aa2d4a3a85c185df7b34559c8544dedd
noob77777 <abhishekpal7@outlook.com>
2024-01-17 19:48:10 +0530

    :add: charges in payment instruments

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,12 +1,9 @@
-import { TransactionStatus } from "@prisma/client";
-import { prisma } from "@server/prisma";
 import {
   addPaymentInstrumentBalance,
   chargePaymentInstrument,
   createPaymentInstrument,
   getPaymentInstrument,
 } from "@server/services/subscription/paymentInstrument";
-import { createTransactionalLock } from "@server/services/subscription/transactionLock";
 import { v4 as uuidv4 } from "uuid";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -14,23 +11,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // userId: "clrghro8f0000ij620r1cgghw"
     // paideia: "1fd6e032e8476c4aa54c18c1a308dce83940e8f4a28f576440513ed7326ad489"
-    // pi: "clrgi50yj0007ij625396kvk9" / ERG
-    // pi: "clrgmgla20009ij623t4je79n" / Paideia
     // const test = await createPaymentInstrument({
     //   userId: "clrghro8f0000ij620r1cgghw",
     //   tokenId: null,
     // });
     // const test = await chargePaymentInstrument({
-    //   paymentInstrumentId: "clrgqu6ix0001fis629920rc5",
+    //   paymentInstrumentId: "clrhujnnz0001t8jgs32y5qpp",
     //   amount: 1000000,
     //   tokenId: null,
     //   idempotencyKey: uuidv4(),
     // });
-    const test = await getPaymentInstrument("clrgmgla20009ij623t4je79n")
+    const test = await getPaymentInstrument("clrhujnnz0001t8jgs32y5qpp")
     // const test = await addPaymentInstrumentBalance({
-    //   paymentInstrumentId: "clrgmgla20009ij623t4je79n",
+    //   paymentInstrumentId: "clrhujnnz0001t8jgs32y5qpp",
     //   address: "9i6UmaoJKWHgWkuq1EJUoYu2hrkRkxAYwQjDotHRHfGrBo16Rss",
-    //   amount: 1000000
+    //   amount: 2000000
     // });
     return res.status(200).json(parse(test));
   } catch (e: any) {
--- src/server/services/subscription/transactionLock.ts
+++ src/server/services/subscription/transactionLock.ts
@@ -1,18 +1,28 @@
 import { prisma } from "@server/prisma";
 import { v4 as uuidv4 } from "uuid";
 
-export const createTransactionalLock = async (id: string) => {
-  const lock = await prisma.transactionalLock.create({
-    data: {
-      id: id,
-    },
+const createTransactionalLockIfRequired = async (id: string) => {
+  const lock = await prisma.transactionalLock.findFirst({
+    where: { id },
   });
+  if (lock === null) {
+    const lock = await prisma.transactionalLock.create({
+      data: {
+        id: id,
+      },
+    });
+    return lock;
+  }
   return lock;
 };
 
 const DEFAULT_LEASE = 60 * 1000; // 1 min
 
 export const acquireTransactionalLock = async (id: string) => {
+  // create the lock if required
+  await createTransactionalLockIfRequired(id);
+
+  // try to make a conditional update on the lock
   const lock = uuidv4();
   const acquired = await prisma.transactionalLock.updateMany({
     where: {
@@ -25,12 +35,18 @@ export const acquireTransactionalLock = async (id: string) => {
       lease: new Date(Date.now() + DEFAULT_LEASE),
     },
   });
+
+  // if the the update is successful, we have the lock. woohoo!!!
   if (acquired.count >= 1) {
     return lock;
   }
+
+  // in case of a bad process we do not want the lock to be held forever
+  // so release locks automatically if the lease has expired
   const transactionalLock = await prisma.transactionalLock.findFirst({
     where: { id },
   });
+  // ugly trick to avoid a null check
   if ((transactionalLock?.lease?.getTime() ?? Date.now() + 1000) < Date.now()) {
     await prisma.transactionalLock.updateMany({
       where: { id },
@@ -41,10 +57,13 @@ export const acquireTransactionalLock = async (id: string) => {
       },
     });
   }
+
+  // throw an error
   throw new Error(`Could not acquire TransactionalLock ${id}.`);
 };
 
 export const releaseTransactionalLock = async (id: string, lock: string) => {
+  // release the lock if we have it
   await prisma.transactionalLock.updateMany({
     where: {
       id,
@@ -56,5 +75,7 @@ export const releaseTransactionalLock = async (id: string, lock: string) => {
       lease: null,
     },
   });
+  // return true anyway as if doesn't matter if
+  // the lock was already released or not held by us
   return true;
 };
