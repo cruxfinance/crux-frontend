1a8d7a8ca6bc858cb6d04262c4727b9ef62f5a44
noob77777 <abhishekpal7@outlook.com>
2024-01-18 20:01:14 +0530

    :add: tokenPrice util

--- src/server/utils/tokenPrice.ts
+++ src/server/utils/tokenPrice.ts
@@ -1,10 +1,30 @@
+import { cruxApi } from "@server/services/axiosInstance";
+
 const ERG_DECIMALS = 9;
 
 // Token USD Price Info
 export const getTokenPriceInfo = async (tokenId: string | null) => {
-  return {
-    tokenId: tokenId,
-    tokenPrice: 1.4,
-    tokenDecimals: 9,
-  };
+  try {
+    const ergPrice = (await cruxApi.get("/coingecko/erg_price")).data.price;
+    if (tokenId === null) {
+      return {
+        tokenId: tokenId,
+        tokenPrice: ergPrice,
+        tokenDecimals: ERG_DECIMALS,
+      };
+    }
+    const tokenInfo = (await cruxApi.get(`/crux/token_info/${tokenId}`)).data;
+    const tokenPrice = tokenInfo.value_in_erg * ergPrice;
+    const tokenDecimals = tokenInfo.decimals;
+    return {
+      tokenId: tokenId,
+      tokenPrice: tokenPrice,
+      tokenDecimals: tokenDecimals,
+    };
+  } catch (e: any) {
+    console.log(e);
+    throw new Error(
+      "Token price API in facing availability issues. Try again later."
+    );
+  }
 };
