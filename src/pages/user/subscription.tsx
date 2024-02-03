36081f32434f7d7c6945249c1f88b8648398c84d
noob77777 <abhishekpal7@outlook.com>
2024-02-03 10:54:17 +0530

    :rebase:

--- src/pages/user/subscription.tsx
+++ src/pages/user/subscription.tsx
@@ -1,20 +1,18 @@
 import SideMenu from "@components/layout/SideMenu";
 import { Box, Link, Paper, Typography } from "@mui/material";
 import { NextPage } from "next";
-import userNavItems from "@lib/navigation/userNav"
+import userNavItems from "@lib/navigation/userNav";
 
 const Subscriptions: NextPage = () => {
   return (
     <SideMenu title="Settings" navItems={userNavItems}>
+      <Typography variant="h5" sx={{ fontWeight: 700, mb: 2 }}>
+        Manage Subscriptions
+      </Typography>
       <Paper sx={{ p: 3 }}>
-        <Typography variant="h5" sx={{ fontWeight: 700, mb: 2 }}>Manage Subscriptions</Typography>
-        <Typography>
-          Coming soon.
-        </Typography>
+        <Typography>Coming soon.</Typography>
         <Link href="/">
-          <Typography>
-            Go Back Home
-          </Typography>
+          <Typography>Go Back Home</Typography>
         </Link>
       </Paper>
     </SideMenu>
