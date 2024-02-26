e8b26cfceae0e10876f5b1df305dada5d7da52a5
noob77777 <abhishekpal7@outlook.com>
2024-02-26 18:02:28 +0530

    :add: notifications trpc routing to notifications menu

--- src/server/services/notification/notification.ts
+++ src/server/services/notification/notification.ts
@@ -25,3 +25,50 @@ export const createNotification = async (input: CreateNotification) => {
   );
   return notification;
 };
+
+export const getNotification = async (notificationId: string) => {
+  const notification = await prisma.notification.findFirst({
+    where: {
+      id: notificationId,
+    },
+  });
+  if (notification === null) {
+    throw new Error(`Notification ${notificationId} not found.`);
+  }
+  return notification;
+};
+
+export const getNotifications = async (userId: string) => {
+  const notifications = await prisma.notification.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+  return notifications;
+};
+
+export const markNotifcationAsRead = async (notificationId: string) => {
+  const notification = await getNotification(notificationId);
+  const update = await prisma.notification.update({
+    where: {
+      id: notificationId,
+    },
+    data: {
+      ...notification,
+      read: true,
+    },
+  });
+  return update;
+};
+
+export const markAllAsRead = async (userId: string) => {
+  const update = await prisma.notification.updateMany({
+    where: {
+      userId: userId,
+    },
+    data: {
+      read: true,
+    },
+  });
+  return update;
+};
